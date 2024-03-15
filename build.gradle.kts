7454b6a0e9538231fd9eaa0557314c3be29ae01c
Daisy <ysiad926@gmail.com>
2024-03-15 11:30:47 +0000

    Project setup

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.seleniumhq.selenium:selenium-java:4.15.0")
+    testImplementation("org.seleniumhq.selenium:selenium-http-jdk-client:4.13.0")
+    testImplementation("io.github.bonigarcia:webdrivermanager:5.6.1")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
