08d15efa7d6568932aa8949f7ca5d62c49467e75
Daisy <ysiad926@gmail.com>
2024-03-22 18:05:24 +0000

    Page Object Models ToBeContinued

---
+++ src/test/java/BbcLocalNewsPage.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BbcLocalNewsPage {
+    protected WebDriver driver;
+    private By searchBoxBy = By.id("ls-c-search__input-label");
+    private By submitInputBy = By.cssSelector(".ls-c-search__submit");
+    private By searchResultsHeadingBy = By.id("main-heading");
+
+    public BbcLocalNewsPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void navigate(){
+        driver.get("https://www.bbc.co.uk/news/localnews");
+    }
+
+    public void searchFor(String searchTerm){
+        WebElement searchBox = driver.findElement((searchBoxBy));
+        searchBox.click();
+        searchBox.sendKeys(searchTerm);
+        driver.findElement(submitInputBy).click();
+
+    }
+
+    public String getSearchResultsHeading(){
+        return driver.findElement(searchResultsHeadingBy).getText();
+    }
+
+    public void waitForResultsText(String resultsText) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.textToBe(searchResultsHeadingBy,resultsText));
+    }
+
+
+    @Test
+    public void shouldFindSearchResult(){
+
+        BbcLocalNewsPage searchPage = new BbcLocalNewsPage(driver);
+        searchPage.navigate();
+        searchPage.searchFor("Guildford");
+        searchPage.waitForResultsText("Guildford");
+        assertEquals("Guildford",searchPage.getSearchResultsHeading());
+
+    }
+}
